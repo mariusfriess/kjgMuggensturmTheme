78573b9e71882385c91a44b1fe1fd67b8e14a8b3
mariusfriess <marius.friess@gmail.com>
2019-08-06 21:17:13 +0200

    footer links update

--- footer.php
+++ footer.php
@@ -2,27 +2,27 @@
   <div class="footer-links">
     <div class="link-box">
       <span>KjG Verbände</span>
-      <a href="#">KjG Bundesebene</a>
-      <a href="#">KjG Diözesanverband Freiburg</a>
-      <a href="#">KjG Dekanat Rastatt</a>
-      <a href="#">BDJK</a>
-      <a href="#">Kath. Jugendbüro Rastatt</a>
-      <a href="#">Seelenbohrerland – der KjG-Webring</a>
+      <a href="https://kjg.de/">KjG Bundesebene</a>
+      <a href="https://www.kjg-freiburg.de/">KjG Diözesanverband Freiburg</a>
+      <a href="https://www.facebook.com/KjG-Dekanat-Rastatt-375914905752231/">KjG Dekanat Rastatt</a>
+      <a href="https://www.bdkj.de/">BDJK</a>
+      <a href="https://rastatt.kja-freiburg.de/">Kath. Jugendbüro Rastatt</a>
+      <a href="https://seelenbohrerland.de/">Seelenbohrerland – der KjG-Webring</a>
     </div>
     <div class="link-box">
       <span>KjG im Dekanat Rastatt</span>
-      <a href="#">KjG Kuppenheim-Oberndorf</a>
-      <a href="#">KjG Ötigheim</a>
-      <a href="#">KjG Rastatt/Rheinau</a>
-      <a href="#">KjG Steinmauern</a>
-      <a href="#">KjG Bietigheim</a>
-      <a href="#">KjG Gaggenau</a>
+      <a href="https://kjgoberndorf.wixsite.com/home">KjG Kuppenheim-Oberndorf</a>
+      <a href="https://www.kjg-oetigheim.org/index.php">KjG Ötigheim</a>
+      <a href="https://www.kjg-rheinau.de/">KjG Rastatt/Rheinau</a>
+      <a href="http://www.kjgsteinmauern.epagede.com/">KjG Steinmauern</a>
+      <a href="https://www.kjgbietigheim.de/">KjG Bietigheim</a>
+      <a href="http://www.kjg-gaggenau.de/">KjG Gaggenau</a>
     </div>
     <div class="link-box">
       <span>Pfarrgemeinde & Förderverein</span>
-      <a href="#">Katholische Kirchengemeinde Vorderes Murgtal</a>
-      <a href="#">Pfarrgemeinde “Maria Königin der Engel” Muggensturm</a>
-      <a href="#">Förderverein der KjG Muggensturm</a>
+      <a href="https://www.vorderes-murgtal.de/">Katholische Kirchengemeinde Vorderes Murgtal</a>
+      <a href="https://www.vorderes-murgtal.de/html/content/maria_koenigin_der_engel_muggensturm.html">Pfarrgemeinde “Maria Königin der Engel” Muggensturm</a>
+      <a href="http://www.kjg-fv-muggensturm.de/">Förderverein der KjG Muggensturm</a>
     </div>
   </div>
   <div class="footer-bottom">
