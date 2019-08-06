7734d6693ae0f212e92cabaca7b2b1e74ff07578
mariusfriess <marius.friess@gmail.com>
2019-08-06 21:18:42 +0200

    footer correction

--- footer.php
+++ footer.php
@@ -2,27 +2,27 @@
   <div class="footer-links">
     <div class="link-box">
       <span>KjG Verbände</span>
-      <a href="https://kjg.de/">KjG Bundesebene</a>
-      <a href="https://www.kjg-freiburg.de/">KjG Diözesanverband Freiburg</a>
-      <a href="https://www.facebook.com/KjG-Dekanat-Rastatt-375914905752231/">KjG Dekanat Rastatt</a>
-      <a href="https://www.bdkj.de/">BDJK</a>
-      <a href="https://rastatt.kja-freiburg.de/">Kath. Jugendbüro Rastatt</a>
-      <a href="https://seelenbohrerland.de/">Seelenbohrerland – der KjG-Webring</a>
+      <a href="https://kjg.de/" target="_blank">KjG Bundesebene</a>
+      <a href="https://www.kjg-freiburg.de/" target="_blank">KjG Diözesanverband Freiburg</a>
+      <a href="https://www.facebook.com/KjG-Dekanat-Rastatt-375914905752231/" target="_blank">KjG Dekanat Rastatt</a>
+      <a href="https://www.bdkj.de/" target="_blank">BDJK</a>
+      <a href="https://rastatt.kja-freiburg.de/" target="_blank">Kath. Jugendbüro Rastatt</a>
+      <a href="https://seelenbohrerland.de/" target="_blank">Seelenbohrerland – der KjG-Webring</a>
     </div>
     <div class="link-box">
       <span>KjG im Dekanat Rastatt</span>
-      <a href="https://kjgoberndorf.wixsite.com/home">KjG Kuppenheim-Oberndorf</a>
-      <a href="https://www.kjg-oetigheim.org/index.php">KjG Ötigheim</a>
-      <a href="https://www.kjg-rheinau.de/">KjG Rastatt/Rheinau</a>
-      <a href="http://www.kjgsteinmauern.epagede.com/">KjG Steinmauern</a>
-      <a href="https://www.kjgbietigheim.de/">KjG Bietigheim</a>
-      <a href="http://www.kjg-gaggenau.de/">KjG Gaggenau</a>
+      <a href="https://kjgoberndorf.wixsite.com/home" target="_blank">KjG Kuppenheim-Oberndorf</a>
+      <a href="https://www.kjg-oetigheim.org/index.php" target="_blank">KjG Ötigheim</a>
+      <a href="https://www.kjg-rheinau.de/" target="_blank">KjG Rastatt/Rheinau</a>
+      <a href="http://www.kjgsteinmauern.epagede.com/" target="_blank">KjG Steinmauern</a>
+      <a href="https://www.kjgbietigheim.de/" target="_blank">KjG Bietigheim</a>
+      <a href="http://www.kjg-gaggenau.de/" target="_blank">KjG Gaggenau</a>
     </div>
     <div class="link-box">
       <span>Pfarrgemeinde & Förderverein</span>
-      <a href="https://www.vorderes-murgtal.de/">Katholische Kirchengemeinde Vorderes Murgtal</a>
-      <a href="https://www.vorderes-murgtal.de/html/content/maria_koenigin_der_engel_muggensturm.html">Pfarrgemeinde “Maria Königin der Engel” Muggensturm</a>
-      <a href="http://www.kjg-fv-muggensturm.de/">Förderverein der KjG Muggensturm</a>
+      <a href="https://www.vorderes-murgtal.de/" target="_blank">Katholische Kirchengemeinde Vorderes Murgtal</a>
+      <a href="https://www.vorderes-murgtal.de/html/content/maria_koenigin_der_engel_muggensturm.html" target="_blank">Pfarrgemeinde “Maria Königin der Engel” Muggensturm</a>
+      <a href="http://www.kjg-fv-muggensturm.de/" target="_blank">Förderverein der KjG Muggensturm</a>
     </div>
   </div>
   <div class="footer-bottom">
