aed8bfd8c2f1b02410bc91f69c53a53ddf0ebce3
mariusfriess <friess.marius@gmail.com>
2020-06-06 22:11:26 +0200

    updated styles

--- 404.php
+++ 404.php
@@ -2,6 +2,9 @@
 get_header();
 ?>
 <div class="not-found">
-  <h1>Diese Seite wurde nicht gefunden</h1>
+  <h1>Fehler 404 - Diese Seite wurde nicht gefunden</h1>
   <p>Die Seite nach der Sie gesucht haben konnte nicht gefunden werden. Eventuell wurde sie gelöscht oder an einen anderen Platz verschoben.</p>
 </div>
+<?php
+get_footer();
+?>
